489441737885353bbe20ffa94a1d953284cffa45
Jay Han <127133985+jay1han@users.noreply.github.com>
2023-05-19 19:16:46 +0200

    Update clearserver.sh for only server

--- clearserver.sh
+++ clearserver.sh
@@ -2,13 +2,4 @@
 
 ./killjava.sh
 echo > /etc/actimetre/server.log
-echo > /etc/actimetre/central.log
-echo > /etc/actimetre/acticentral.lock
-rm -f /media/actimetre/Data/*
-rm -f /media/actimetre/Repo/*
-echo {} > /etc/actimetre/registry.data
-echo {} > /etc/actimetre/actiservers.data
-echo {} > /etc/actimetre/actimetres.data
 echo {} > /etc/actimetre/self.data
-echo {} > /etc/actimetre/meta.data
-rm -f /etc/actimetre/acticentral.pid
