40d241d2a98fa447511cae32129d6dfc50eb050a
Jay Han <127133985+jay1han@users.noreply.github.com>
2023-05-19 19:16:21 +0200

    Update install.sh for just Actiserver

--- install.sh
+++ install.sh
@@ -1,35 +1,13 @@
 #!/usr/bin/bash
 
 mkdir /etc/actimetre
-mkdir /media/actimetre
-mkdir /media/actimetre/Data
-mkdir /media/actimetre/Repo
 mkdir /var/www/cgi-bin
 
-cp actimetre.conf clear2.sh killjava.sh run.sh /etc/actimetre
-cp cgi-bin/acticentral.py /var/www/cgi-bin/acticentral.py
-cp html/index.html html/error.html /var/www/html/
-
-cd /media/actimetre
-chown www-data:www-data . Data Repo
-chmod 777 . Data Repo
-rm -f /media/actimetre/Data/*
-rm -f /media/actimetre/Repo/*
-
-cd /var/www
-chown www-data:www-data html/index.html html/error.html cgi-bin/acticentral.py
-chmod 775 html/index.html cgi-bin/acticentral.py
+cp actimetre.conf clearserver.sh killjava.sh run.sh /etc/actimetre
 
 cd /etc/actimetre
 chown www-data:www-data .
 chmod 777 . *.sh
 echo > server.log
-echo > central.log
-echo > acticentral.lock
-echo {} > registry.data
-echo {} > actiservers.data
-echo {} > actimetres.data
 echo {} > self.data
-echo {} > meta.data
-rm -f acticentral.pid
-chmod 666 *.log *.data *.lock *.conf
+chmod 666 *.log *.data
